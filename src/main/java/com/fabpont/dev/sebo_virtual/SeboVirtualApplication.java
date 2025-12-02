0abca9b87fb41f7d29103258efe8f1c954517397
Fabpont <fabriciopontes02.fa@gmail.com>
2025-12-02 09:47:27 -0300

    Starting a new book website project

---
+++ src/main/java/com/fabpont/dev/sebo_virtual/SeboVirtualApplication.java
@@ -0,0 +1,13 @@
+package com.fabpont.dev.sebo_virtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeboVirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeboVirtualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fabpont/dev/sebo_virtual/SeboVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabpont.dev.sebo_virtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeboVirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
