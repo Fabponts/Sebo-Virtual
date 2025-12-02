1e9a45ad37a2c89605f16b74b4c12088d79bed6b
Fabpont <fabriciopontes02.fa@gmail.com>
2025-12-02 11:00:48 -0300

    feat(db_migration): fix migrations sql erros

--- src/main/resources/db/migration/V1__new__tables.sql
+++ src/main/resources/db/migration/V1__new__tables.sql
@@ -1,26 +1,29 @@
 CREATE TABLE users (
-    id Long PRIMARY KEY,
+    id BIGINT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
     password VARCHAR(20) NOT NULL,
     phone_number VARCHAR(20) NOT NULL,
-    address VARCHAR (100) NOT NULL
-)
-CREATE TABLE books(
-    id Long PRIMARY KEY,
+    address VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE books (
+    id BIGINT PRIMARY KEY,
     title VARCHAR(50) NOT NULL,
     author VARCHAR(50) NOT NULL,
     sinopse VARCHAR(500) NOT NULL,
     genre VARCHAR(20) NOT NULL,
-    release_year DATE NOT NULL,
+    release_year DATE NOT NULL
+);
 
-)
-CREATE TABLE ad(
-    id Long PRIMARY KEY,
-    book_id Long FOREIGN KEY,
-    user_id Long FOREIGN KEY,
-    price DOUBLE NOT NULL,
+CREATE TABLE ad (
+    id BIGINT PRIMARY KEY,
+    book_id BIGINT NOT NULL,
+    user_id BIGINT NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
     book_condition VARCHAR(20) NOT NULL,
     ad_date DATE NOT NULL,
-    
-)
+
+    FOREIGN KEY (book_id) REFERENCES books(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
